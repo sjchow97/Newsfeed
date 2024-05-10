28c0947fadcc079b4758b9985183fbd3c0db5af6
lestershx <lshun@my.bcit.ca>
2024-05-10 01:29:44 -0700

    implemented a uuid convertor to save reference_id's

--- rss/views.py
+++ rss/views.py
@@ -1,29 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from django.template.defaultfilters import slugify
 from .feed_reader import FeedReader
 from .comment_model_manager import CommentModelManager
 from .reaction_model_manager import ReactionModelManager
 
-from .models import PostReference, PostComment, PostReaction
+from .models import PostReference
 
+import uuid
 
 # Create your views here.
+
+REFERENCE_NAMESPACE = uuid.UUID('6ba7b810-9dad-11d1-80b4-00c04fd430c8')
 
 def index(request):
     feeds = FeedReader.get_feeds(request.user.userprofile.location)
     comment_dict = {}
     reaction_dict = {}
     for entry in feeds.entries:
-        reference_id = entry.title
-        slugified_title = slugify(reference_id)
+        reference_id = uuid.uuid5(REFERENCE_NAMESPACE, entry.title.encode('utf-8'))
+        print("id: " + str(reference_id))
         if PostReference.objects.filter(reference_id=reference_id).exists():
+            print("Reference exists")
             reference = PostReference.objects.get(reference_id=reference_id)
             comments = CommentModelManager.get_comments(reference)
             reactions = ReactionModelManager.get_reactions(reference)
-            comment_dict[slugified_title] = comments
-            reaction_dict[slugified_title] = reactions
+            comment_dict[reference_id] = comments
+            reaction_dict[reference_id] = reactions
     template = loader.get_template("rss/index.html")
     context = {
         "feeds": feeds,
