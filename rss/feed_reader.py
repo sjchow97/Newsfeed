83b7d731d25dd96f0c5228051e0d5bcb4c7dbe49
lestershx <lshun@my.bcit.ca>
2024-05-10 12:59:33 -0700

    changed comment_model_manager, feed_reader, and reaction_model_manager to be standalone functions and not classes since they do not need to have a state, they can just be standalone functions

--- rss/feed_reader.py
+++ rss/feed_reader.py
@@ -3,19 +3,17 @@ from .models import RssSource
 
 import feedparser
 
-class FeedReader(object):
-    @staticmethod
-    def get_feeds(location):
-        rss_sources = RssSource.objects.filter(location=location)
-        feeds = feedparser.FeedParserDict()
-        feeds['entries'] = []
+def get_feeds(location):
+    rss_sources = RssSource.objects.filter(location=location)
+    feeds = feedparser.FeedParserDict()
+    feeds['entries'] = []
 
-        for source in rss_sources:
-            feed = feedparser.parse(source.url)
-            if feed.bozo:
-                print("Error reading feed: " + feed.bozo_exception)
-                continue
-            else:
-                feeds['entries'].extend(feed.entries)
-        feeds['entries'].sort(key=attrgetter('published_parsed'), reverse=True)
-        return feeds
+    for source in rss_sources:
+        feed = feedparser.parse(source.url)
+        if feed.bozo:
+            print("Error reading feed: " + feed.bozo_exception)
+            continue
+        else:
+            feeds['entries'].extend(feed.entries)
+    feeds['entries'].sort(key=attrgetter('published_parsed'), reverse=True)
+    return feeds
--- rss/reaction_model_manager.py
+++ rss/reaction_model_manager.py
@@ -1,15 +1,12 @@
 from .models import PostReaction
 
-class ReactionModelManager:
-    @staticmethod
-    def get_reactions(reference_id):
-        return PostReaction.objects.filter(reference=reference_id)
+def get_reactions(reference_id):
+    return PostReaction.objects.filter(reference=reference_id)
 
-    @staticmethod
-    def add_reaction(reference_id, user, vote):
-        reaction = PostReaction()
-        reaction.reference = reference_id
-        reaction.user = user
-        reaction.vote = vote
-        reaction.save()
-        return reaction
+def add_reaction(reference_id, user, vote):
+    reaction = PostReaction()
+    reaction.reference = reference_id
+    reaction.user = user
+    reaction.vote = vote
+    reaction.save()
+    return reaction
