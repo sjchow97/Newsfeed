419fe4ceb55efcfbdc336f64858ff90634c83992
Stanley Chow <sjchow97@gmail.com>
2024-05-13 16:05:46 -0700

    added additional logic to find rss feeds from urls

--- scraper/recursiveScraper.py
+++ scraper/recursiveScraper.py
@@ -2,23 +2,38 @@ import requests
 from bs4 import BeautifulSoup
 from urlparse import urljoin, urlparse
 
+checked_urls = set()
+found_rss_feeds = set()
+
 def find_rss_feed(url):
+    global checked_urls
+    global found_rss_feeds
     try:
+        if url in checked_urls:
+            print('URL already checked:', url)
+            return
+        checked_urls.add(url)
+
         response = requests.get(url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.content, 'html.parser')
-            # Look for <link> tags with rel="alternate" and type="application/rss+xml"
-            rss_links = soup.find_all('link', rel='alternate', type='application/rss+xml')
+            # Look for <link> and <a> tags
+            rss_links = soup.find_all(['link', 'a'])
             if rss_links:
                 for link in rss_links:
                     rss_url = link.get('href')
-                    print 'RSS feed found on {}: {}'.format(url, urljoin(url, rss_url))
+                    # Check if the URL contains "rss" or "feed/" (important to check for feed/ otherwise common URLs like /feedback will be included as well)
+                    if rss_url and ('rss' in rss_url.lower() or 'feed/' in rss_url.lower()):
+                        full_rss_url = urljoin(url, rss_url)
+                        if full_rss_url not in found_rss_feeds:
+                            found_rss_feeds.add(full_rss_url)
+                            print('RSS feed found on {}: {}'.format(url, full_rss_url))
             else:
-                print 'No RSS feed found on:', url
+                print('No RSS feed found on:', url)
         else:
-            print 'Failed to fetch {}: Status code {}'.format(url, response.status_code)
+            print('Failed to fetch {}: Status code {}'.format(url, response.status_code))
     except Exception as e:
-        print 'Error occurred while fetching {}: {}'.format(url, e)
+        print('Error occurred while fetching {}: {}'.format(url, e))
 
 def crawl_website(start_url):
     visited_urls = set()
@@ -42,10 +57,11 @@ def crawl_website(start_url):
             
 
                         # Ensures that the script only check the URLs within the same domain
+                        #if absolute_url.startswith(start_url) and any(substring in parsed_url.path for substring in ["feed", "rss", "xml"]):
                         if absolute_url.startswith(start_url):
                             queue.append(absolute_url)
             except Exception as e:
                 print 'Error occurred while crawling {}: {}'.format(url, e)
 
 # Example usage:
-crawl_website('https://www.coquitlam.ca/')
+crawl_website('https://vancouver.ca/')
