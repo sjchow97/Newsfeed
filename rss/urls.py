1d991ebfc92f7d827e6d4606850624e14c5476e1
lestershx <lshun@my.bcit.ca>
2024-05-15 10:37:22 -0700

    added url endpoints for comment CRUD methods

--- rss/urls.py
+++ rss/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = "rss"
 urlpatterns = [
     url("", views.index, name="index"),
+    url("post_comment/", views.post_comment, name="post_comment"),
+    url("edit_comment/", views.edit_comment, name="edit_comment"),
+    url("delete_comment/", views.delete_comment, name="delete_comment"),
 ]
