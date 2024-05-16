11a48408d3d7d47c5973e0eef482585750935d6d
Stanley Chow <sjchow97@gmail.com>
2024-05-16 14:58:36 -0700

    created sample csv with fewer municipalities, connected csv parsing script with recursive crawler

--- scraper/recursiveScraper.py
+++ scraper/recursiveScraper.py
@@ -4,11 +4,12 @@ from urlparse import urljoin, urlparse
 import urllib2
 import HTMLParser
 import re
+import csv
 
 checked_urls = set()
 found_rss_feeds = set()
 
-def find_rss_feed(url):
+def find_rss_feed(url, municipality):
     global checked_urls
     global found_rss_feeds
     try:
@@ -48,7 +49,7 @@ def find_rss_feed(url):
                                         print("BASE URL: " + full_rss_url)
                                         print("NAME: " + soup.title.string)
                                         # location needs to be based on csv file inputs
-                                        print("LOCATION: ")
+                                        print("LOCATION: " + municipality)
                                       
                                         # parse each <item> tag for fields that will be displayed on the news feed
                                         for item in soup.find_all('item'):
@@ -61,9 +62,10 @@ def find_rss_feed(url):
                                             unescaped_title = (HTMLParser.HTMLParser().unescape(title.get_text())).encode('utf-8')
                                             print "Unescaped title: " + unescaped_title
                                             
-                                            # should probably not include items that do not have a pubdate
+                                            # should probably not include items that do not have a pubdate onto the news feed
                                             if pub_date == None:
-                                                print "No publication date found"
+                                                print "No publication date found for this news item, skipping to next item"
+                                                continue
                                             else:
                                                 print "Publication date: " + pub_date.get_text()
                                         
@@ -74,7 +76,6 @@ def find_rss_feed(url):
                                             print "Link: " + link.get_text()
 
                                             #this works
-                                            
                                             unescaped_description = (HTMLParser.HTMLParser().unescape(description)).encode('utf-8')
                                             print "Unescaped description: " + unescaped_description
                                             
@@ -145,14 +146,14 @@ def find_rss_feed(url):
     except Exception as e:
         print('Error occurred while fetching {}: {}'.format(url, e))
 
-def crawl_website(start_url):
+def crawl_website(start_url, municipality):
     visited_urls = set()
     queue = [start_url]
 
     while queue:
         url = queue.pop(0)
         if url not in visited_urls:
-            find_rss_feed(url)
+            find_rss_feed(url, municipality)
             visited_urls.add(url)
             try:
                 response = requests.get(url)
@@ -174,4 +175,42 @@ def crawl_website(start_url):
                 print 'Error occurred while crawling {}: {}'.format(url, e)
 
 # Example usage:
-crawl_website('https://vancouver.ca/')
+#crawl_website('https://vancouver.ca/')
+
+
+with open('sample.csv', 'r') as file:
+    # Create a CSV reader object
+    csv_reader = csv.reader(file)
+    
+    count = 0
+
+    for row in csv_reader:
+
+        count+=1
+
+        print count
+        municipality = row[0]
+        if " " in municipality:
+            municipality = municipality.replace(" ", "_")
+        province = row[1]
+        if " " in province:
+            province = province.replace(" ", "_")
+        
+        wiki_site = "https://en.wikipedia.org/wiki/{},_{}".format(municipality, province)
+
+        try:
+            response = urllib2.urlopen(wiki_site)
+
+            if response.getcode() == 200:
+                html = response.read()
+                soup = BeautifulSoup(html, 'html.parser')
+
+                # Find the <a> tag within the <span> tags with class "official-website" and get the href attribute
+                link = soup.find('span', class_='official-website').find('a').get('href')
+                #print link
+                
+                crawl_website(link, municipality)
+
+        except urllib2.HTTPError as e:
+        # If an HTTP error occurs, print the status code and error message
+            print("HTTP Error:", e.code, e.reason)
