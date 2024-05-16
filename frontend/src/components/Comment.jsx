e625dc03cd012963cb534c81438be7aa44332046
Brandon Chan <brandonchan45@gmail.com>
2024-05-16 13:55:48 -0700

    basic comment frontend

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Comment = ({ comment }) => {
+const Comment = () => {
+  const [comments, setComments] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("http://127.0.0.1:8000/api/");
+        const data = await res.json();
+        setComments(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="comment">
-      <h3>{comment.name}</h3>
-      <p>{comment.body}</p>
+    <div>
+      {comments.map((comment) => (
+        <div key={comment.comment_id}>
+          <h1>{comment.post_title}</h1>
+          <p>{comment.content}</p>
+        </div>
+      ))}
     </div>
   );
 };
