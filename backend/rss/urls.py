6800abe8d1eeaf505688b209ba80df85432d7301
lestershx <lshun@my.bcit.ca>
2024-05-17 11:59:05 -0700

    added documentation comments on views.py and feed_reader.py, and cleaned up deprecated code

--- backend/rss/urls.py
+++ backend/rss/urls.py
@@ -4,12 +4,9 @@ from . import views
 
 app_name = "rss"
 urlpatterns = [
-    # url("", views.index, name="index"),
-    # url("post_comment/", views.post_comment, name="post_comment"),
-    # url("edit_comment/", views.edit_comment, name="edit_comment"),
-    # url("delete_comment/", views.delete_comment, name="delete_comment"),
     url(r'^post_comment/$', views.create_post_comment, name="post_comment"),
     url(r'^edit_comment/(?P<comment_id>\d+)/$', views.edit_post_comment, name="edit_comment"),
     url(r'^delete_comment/(?P<comment_id>\d+)/$', views.delete_post_comment, name="delete_comment"),
     url(r'^list_comments/$', views.list_comments, name="list_comments"),
+    url(r'^read_feeds/', views.read_feeds, name="read_feeds"),
 ]
--- backend/rss/views.py
+++ backend/rss/views.py
@@ -3,97 +3,50 @@ from django.http import HttpResponse
 from django.template import loader
 from django.core.exceptions import ValidationError
 from django.utils import timezone
-
-from .feed_reader import get_feeds
-from .comment_model_manager import get_comments, add_comment, edit_comment, delete_comment
-from .reaction_model_manager import get_reactions
-
-from .models import PostReference
-
-import uuid
-
-# Create your views here.
-
-# REFERENCE_NAMESPACE = uuid.UUID('6ba7b810-9dad-11d1-80b4-00c04fd430c8')
-
-# def index(request):
-#     feeds = get_feeds(request.user.userprofile.location)
-#     comment_dict = {}
-#     reaction_dict = {}
-#     for entry in feeds.entries:
-#         reference_id = uuid.uuid5(REFERENCE_NAMESPACE, entry.title.encode('utf-8'))
-#         print("id: " + str(reference_id))
-#         if PostReference.objects.filter(reference_id=reference_id).exists():
-#             print("Reference exists")
-#             reference = PostReference.objects.get(reference_id=reference_id)
-#             comments = get_comments(reference)
-#             reactions = get_reactions(reference)
-#             comment_dict[reference_id] = comments
-#             reaction_dict[reference_id] = reactions
-#     template = loader.get_template("rss/index.html")
-#     context = {
-#         "feeds": feeds,
-#         "post_comments": comment_dict,
-#         "post_reactions": reaction_dict,
-#     }
-#     return HttpResponse(template.render(context, request))
-
-# # @csrf_exempt
-# def post_comment(request):
-#     if request.method == 'POST':
-#         reference_id = request.POST['reference_id']
-#         post_title = request.POST['post_title']
-#         content = request.POST['content']
-#         user = request.user
-
-#         try: 
-#             add_comment(reference_id, user, post_title, content)
-#             return JsonResponse({'status': 'success', 'message': 'Comment added successfully'})
-#         except ValidationError as e:
-#             return JsonResponse({'status': 'error', 'message': str(e)})
-#     else:
-#         return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
-
-# # @csrf_exempt
-# def edit_comment(request):
-#     if request.method == 'POST':
-#         comment_id = request.POST['comment_id']
-#         post_title = request.POST['post_title']
-#         content = request.POST['content']
-#         user = request.user
-
-#         try: 
-#             edit_comment(comment_id, user, post_title, content)
-#             return JsonResponse({'status': 'success', 'message': 'Comment edited successfully'})
-#         except ValidationError as e:
-#             return JsonResponse({'status': 'error', 'message': str(e)})
-#     else:
-#         return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
-
-# # @csrf_exempt
-# def delete_comment(request):
-#     if request.method == 'POST':
-#         comment_id = request.POST['comment_id']
-#         user = request.user
-
-#         try: 
-#             delete_comment(comment_id, user)
-#             return JsonResponse({'status': 'success', 'message': 'Comment deleted successfully'})
-#         except ValidationError as e:
-#             return JsonResponse({'status': 'error', 'message': str(e)})
-#     else:
-#         return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
+from .feed_reader import get_feeds, feed_to_json
+from .comment_model_manager import get_comments
+from .reaction_model_manager import get_reactions
 from .models import PostReference, PostComment
 from .serializers import PostReferenceSerializer, PostCommentSerializer
 
-# Sample class to perform a GET and POST to the database
-# class ListComments(generics.ListCreateAPIView):
-#     queryset = PostComment.objects.all()
-#     serializer_class = PostCommentSerializer
+import uuid
 
+REFERENCE_NAMESPACE = uuid.UUID('6ba7b810-9dad-11d1-80b4-00c04fd430c8')
+
+# Gets all the RSS feed entries depending on the user's location, as well as the comments and reactions for the related posts
+# GET /rss/read_feeds/
+# params: request object
+# returns: response object with body containing JSON object containing the feed posts, comments, and reactions
+@api_view(['GET'])
+def read_feeds(request):
+    feeds = get_feeds(request.user.userprofile.location)
+    json_feeds = feed_to_json(feeds)
+    comment_dict = {}
+    # reaction_dict = {}
+    for entry in feeds.entries:
+        reference_id = uuid.uuid5(REFERENCE_NAMESPACE, entry.title.encode('utf-8'))
+        if PostReference.objects.filter(reference_id=reference_id).exists():
+            reference = PostReference.objects.get(reference_id=reference_id)
+            comments = get_comments(reference)
+            post_comment_serializer = PostCommentSerializer(comments, many=True)
+            # reactions = get_reactions(reference)
+            comment_dict[str(reference_id)] = post_comment_serializer.data
+            # reaction_dict[str(reference_id)] = reactions
+    context = {
+        "feed_posts": json_feeds["entries"], 
+        "post_comments": comment_dict,
+        # "post_reactions": reaction_dict,
+    }
+    return Response(context)
+
+# Gets all the comments in the database
+# GET /rss/list_comments/
+# params: request object
+# returns: response object with body containing JSON object containing all the comments
 @api_view(['GET'])
 def list_comments(request):
     if request.method == 'GET':
@@ -103,9 +56,14 @@ def list_comments(request):
     else:
         return Response({'error': 'Invalid request method'}, status=405)
 
+# Creates a new comment for a post
+# POST /rss/post_comment/
+# params: request object with body containing JSON object with the post comment data
+# returns: response object with body containing JSON object with the new post comment data
 @api_view(['POST'])
 def create_post_comment(request):
     if request.method == 'POST':
+        # Request body should contain post_reference data of the post they are commenting on
         post_reference_data = request.data.pop('post_reference', None)
         if post_reference_data is not None:
             post_reference, created = PostReference.objects.get_or_create(reference_id=post_reference_data['reference_id'], defaults=post_reference_data)
@@ -125,6 +83,10 @@ def create_post_comment(request):
     else:
         return Response({'error': 'Invalid request method'}, status=405)
 
+# Edits an existing comment
+# PUT /rss/edit_comment/<comment_id>/
+# params: request object with body containing JSON object with the new post comment data
+# returns: response object with body containing JSON object with the edited post comment data
 @api_view(['PUT'])
 def edit_post_comment(request, comment_id):
     if request.method == 'PUT':
@@ -147,6 +109,10 @@ def edit_post_comment(request, comment_id):
     else:
         return Response({'error': 'Invalid request method'}, status=405)
 
+# Deletes an existing comment
+# DELETE /rss/delete_comment/<comment_id>/
+# params: request object
+# returns: response object with body containing JSON object with message indicating success or failure
 @api_view(['DELETE'])
 def delete_post_comment(request, comment_id):
     if request.method == 'DELETE':
