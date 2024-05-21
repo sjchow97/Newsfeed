8d89c575057eb8133da96f5ba4f2d0d3b78614c0
Brandon Chan <brandonchan45@gmail.com>
2024-05-20 23:44:45 -0700

    added code to handle unlogged in users attempting to navigate to /feed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,31 @@ import "./styles/App.css";
 import Login from "./components/Login/Login";
 import FeedPage from "./components/Feed/FeedPage";
 
+import { AuthProvider } from "./context/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
+
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/feed" element={<FeedPage />} />
+        <Route
+          path="/feed"
+          element={
+            <PrivateRoute>
+              <FeedPage />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </div>
   );
 }
 
-export default App;
+const WrappedApp = () => (
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+);
+
+export default WrappedApp;
