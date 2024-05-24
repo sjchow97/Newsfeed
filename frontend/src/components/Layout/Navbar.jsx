81b0d3c2282e3d5572d73f9331376bf9749c108a
Nanak <snanak1403@gmail.com>
2024-05-24 05:22:15 -0700

    Applying previous changes of media queries

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../Images/logo.png";
 import "./Navbar.css";
 
 function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="navbar">
       <img src={logo} alt="logo" className="logo" />
-      <div className="nav-buttons">
+      <div className={`nav-buttons ${showMenu ? "active" : ""}`}>
         <button>Explore Topics</button>
         <button>Start a Conversation</button>
         <button>Home</button>
       </div>
-      <div className="profile-notifications">
-        <button className="notifications-button">Notifications</button>
-        <div className="profile">
-          <img src={logo} alt="profile" />
-        </div>
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <div className={`bar ${showMenu ? "open" : ""}`}></div>
       </div>
     </div>
   );
